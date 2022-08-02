0303bf601d929e3711e5c1d1de6adcac2bdd0e2d
김안나 <yorocobe@gmail.com>
2022-08-02 17:36:33 +0900

    feat: 랜딩 페이지 카드 기능 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,17 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Button, Icon, Col, Card, Row } from "antd";
+import Meta from "antd/lib/card/Meta";
+import ImageSlider from "../../utils/ImageSlider";
 
 function LandingPage() {
+  const [Products, setProducts] = useState([]);
+
   useEffect(() => {
     axios.post("/api/product/products").then((response) => {
       if (response.data.success) {
         console.log(response.data);
+        setProducts(response.data.productInfo);
       } else {
         alert("상품들을 가져오는데 실패 했습니다.");
       }
     });
   }, []);
-  return <div>LandingPage</div>;
+
+  const renderCards = Products.map((product, index) => {
+    console.log(product);
+
+    return (
+      <Col lg={6} md={8} xs={24} key={index}>
+        <Card cover={<ImageSlider images={product.images} />}>
+          <Meta title={product.title} description={`$${product.price}`} />
+        </Card>
+      </Col>
+    );
+  });
+  return (
+    <div style={{ width: "75%", margin: "3rem auto" }}>
+      <div style={{ textAlign: "center" }}>
+        <h2>
+          Let's Travel Anywhere
+          <Icon type="rocket" />
+        </h2>
+      </div>
+
+      {/* Filter */}
+
+      {/* Search */}
+
+      {/* Cards */}
+      <Row gutter={[16, 16]}>{renderCards}</Row>
+
+      <div style={{ justifyContent: "center" }}>
+        <Button>더보기</Button>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
