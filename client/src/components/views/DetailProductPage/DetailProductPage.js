dade97478bf22136e25cce7030e6e97d5fe26874
김안나 <yorocobe@gmail.com>
2022-08-22 18:13:41 +0900

    feat: 상세페이지의 ProductInfo 부분의 컴포넌트 구현

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -33,7 +33,7 @@ function DetailProductPage(props) {
         </Col>
         <Col lg={12} sm={24}>
           {/* ProductInfo */}
-          <ProductInfo />
+          <ProductInfo detail={Product} />
         </Col>
       </Row>
     </div>
