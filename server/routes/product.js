57680db2ddb9e1d009d6476243e195dbb05250de
김안나 <yorocobe@gmail.com>
2022-08-26 19:10:36 +0900

    feat: 카트에 담긴 상품 정보를 DB에서 가져오는 기능 구현

--- server/routes/product.js
+++ server/routes/product.js
@@ -94,13 +94,19 @@ router.post("/products", (req, res) => {
 router.get("/products_by_id", (req, res) => {
   //productId를 이용해서 DB에서 productId와 같은 상품의 정보를 가져온다.
   let type = req.query.type;
-  let productId = req.query.id;
+  let productIds = req.query.id;
 
-  Product.find({ _id: productId })
+  if (type === "array") {
+    let ids = req.query.id.split(",");
+    productIds = ids.map((item) => {
+      return item;
+    });
+  }
+  Product.find({ _id: { $in: productIds } })
     .populate("writer")
     .exec((err, product) => {
-      if (err) return res.status(400).send(err);
-      return res.status(200).send({ success: true, product });
+      if (err) return res.status(400).json(err);
+      return res.status(200).json({ success: true, product });
     });
 });
 
